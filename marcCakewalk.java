5cccf04452daecc95ca2c7cfa90c44d205c82079
jignesh@1001 <jigneshgurav12@gmail.com>
2024-02-11 01:23:17 +0530

    Day10

---
+++ marcCakewalk.java
@@ -0,0 +1,7 @@
+public class marcCakewalk {
+    public static int saveThePrisoner(int n, int m, int s) {
+        // Write your code here
+            return (m + s - 2) % n + 1;
+    
+        }
+}
